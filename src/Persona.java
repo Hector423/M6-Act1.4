55868b8dbeb69438a8341f11583e3d51b0fa901a
Héctor Vallvé <hectorvallveg@gmail.com>
2022-10-19 19:38:31 +0200

    jeje

--- src/Persona.java
+++ src/Persona.java
@@ -1,12 +1,10 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.RandomAccessFile;
+import java.io.*;
+import org.w3c.dom.*;
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.*;
+import javax.xml.transform.stream.*;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.DOMImplementation;
-import org.w3c.dom.Document;
 
 public class Persona {
 
@@ -14,37 +12,78 @@ public class Persona {
 		File fitxer = new File("AleatoriTreb.dat");
 		RandomAccessFile fitxerLectura = new RandomAccessFile(fitxer, "r");
 		
-		int id, dep, posicio=0; 
+			
+		
+		int id, edat, posicio=0; 
 		Double salari;
-		char cognom[] = new char[10], aux;
+		char cognom[] = new char[10], auxCognom;
+		char dni[] = new char[9], auxDni;
+		char nom[] = new char[10], auxNom;
+		
+		
 		
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		
 		try {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			DOMImplementation implementation = builder.getDOMImplementation();
-			Document document = implementation.createDocument(null, "Empleados", null);
+			Document document = implementation.createDocument(null, "empleados", null);
 			document.setXmlVersion("1.0");
 			
 			for(;;) {
 				fitxerLectura.seek(posicio);
 				id=fitxerLectura.readInt();
 				for (int i = 0; i <cognom.length; i++) {
-					aux = fitxerLectura.readChar();
-					
+					auxCognom = fitxerLectura.readChar();
+					cognom[i] = auxCognom;
 				}
+				String cognomS = new String(cognom);
+				
+				for (int i = 0; i<dni.length; i++) {
+					auxDni = fitxerLectura.readChar();
+					dni[i] = auxDni;
+				}
+				String dniS = new String(dni);
+				
+				for (int i = 0; i < nom.length; i++) {
+					auxNom = fitxerLectura.readChar();
+					nom[i] = auxNom;
+				}
+				String nomS = new String(nom);
+				
+				edat = fitxerLectura.readInt();
+				salari = fitxerLectura.readDouble();
+				
+				if(id>0) {
+					Element raiz = document.createElement("empleado");
+					
+					document.getDocumentElement().appendChild(raiz);
+					CrearElemento("id", Integer.toString(id), raiz, document);
+					CrearElemento("cognom", cognomS.trim(), raiz, document);
+					CrearElemento("dni", dniS.trim(), raiz, document);
+					CrearElemento("noms", nomS.trim(), raiz, document);
+					CrearElemento("edad", Integer.toString(edat), raiz, document);
+					CrearElemento("salario", Double.toString(salari), raiz, document);
+				}
+				posicio= posicio+36;
+				if(fitxerLectura.getFilePointer() == fitxerLectura.length()) break;
 			}
 			
-			
-			
-		}catch (Exception e) {
-			// TODO: handle exception
-		}
-		
-		
-		
-		
-
+			Source source = new DOMSource(document);
+			Result result = new StreamResult(new java.io.File("Empleados.xml"));
+			Transformer transformer = TransformerFactory.newInstance().newTransformer();
+			transformer.transform(source, result);
+		}catch (Exception e) { System.out.println("Error: "+e);
+		fitxerLectura.close();
 	}
 
+	}
+	
+	static void CrearElemento(String datoEmple, String valor, 
+			Element raiz, Document document) {
+		Element elem = document.createElement(datoEmple);
+		Text text = document.createTextNode(valor);
+		raiz.appendChild(elem);
+		elem.appendChild(text);
+	}
 }
---
+++ src/crearPersona.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+public class crearPersona {
+
+	public static void main(String[] args)throws IOException {
+		File fitxer = new File("AleatoriTreb.dat");
+		RandomAccessFile accesArxiu = new RandomAccessFile(fitxer, "rw");
+
+		int edats[] = {20, 18, 21, 22, 12, 32, 25, 23};
+		String noms[] = {"Hector", "Jordi", "Eloy", "Xavier", "Antonio", "Jaume", "Cels", "Marta"};
+		String cognoms[] = {"Vallvé", "Ribellas", "Altozano", "Martinez", "Lopez", "Rubio", "Montes", "Cruz"};
+		String dnis[] = {"12345678L", "93647586T", "63193574O", "23145674Z", "95138475G", "66882223U", "83648284I", "43385477J"};
+		Double salaris[] = {2000.0, 1500.0, 1200.0, 1900.0, 1500.0, 1800.0, 1300.0, 2500.0};
+		
+		StringBuffer buffer = null;
+		int n = cognoms.length;
+		
+		for(int i=0; i<n; i++) {
+			accesArxiu.writeInt(i+1);
+			buffer = new StringBuffer(noms[i]);
+			buffer.setLength(10);
+			buffer = new StringBuffer(cognoms[i]);
+			buffer.setLength(10);
+			buffer = new StringBuffer(dnis[i]);
+			buffer.setLength(10);
+			accesArxiu.writeInt(edats[i]);
+			accesArxiu.writeChars(buffer.toString());
+			accesArxiu.writeDouble(salaris[i]);
+		}
+		
+	}
+
+}
